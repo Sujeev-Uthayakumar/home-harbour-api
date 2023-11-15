1ef192d842e0a407565ad09199fd5a48ed0a2973
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-11-15 11:57:58 -0500

    add new auth route and get expiry

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,12 +1,14 @@
 const { admin } = require("../firebase");
 const { db } = require("../firebase");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 const USER_COLLECTION = "users";
 
 router.post("/signup", async (req, res) => {
   const { fullName, email, password, dateOfBirth } = req.body;
+  console.log(req.body);
   try {
     const userRecord = await admin.auth().createUser({
       email,
@@ -14,6 +16,10 @@ router.post("/signup", async (req, res) => {
     });
 
     const customToken = await admin.auth().createCustomToken(userRecord.uid);
+
+    const decodedToken = jwt.decode(customToken);
+    const expirationTime = new Date(decodedToken.exp * 1000);
+    console.log(`Token will expire at ${expirationTime}`);
 
     const documentCollection = db
       .collection(USER_COLLECTION)
@@ -29,6 +35,7 @@ router.post("/signup", async (req, res) => {
 
     res.status(201).send({
       token: customToken,
+      tokenExpires: expirationTime.toISOString(),
       userData: {
         uid: userRecord.uid,
         email,
@@ -40,6 +47,23 @@ router.post("/signup", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(400).send({ error: error.code });
+  }
+});
+
+router.get("/find/:id", async (req, res) => {
+  const { id } = req.params;
+  const documentRef = db.collection(USER_COLLECTION).doc(id);
+
+  try {
+    const doc = await documentRef.get();
+    console.log(doc);
+    if (doc.exists) {
+      res.json({ id: doc.id, ...doc.data() });
+    } else {
+      res.status(404).send("Document not found");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
