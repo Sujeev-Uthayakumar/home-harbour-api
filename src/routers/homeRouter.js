7803ebb3263f99337f784140d3bdc543d252747c
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2023-12-03 18:58:38 -0500

    store images

--- src/routers/homeRouter.js
+++ src/routers/homeRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { db } = require("../firebase");
+const { db, bucket } = require("../firebase");
+const multer = require("multer");
 
 const Home = require("../models/Home");
 
 const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const HOME_COLLECTION = "homes";
 
 router.get("/findAll", async (req, res) => {
@@ -54,17 +57,58 @@ router.put("/update/:id", async (req, res) => {
 });
 
 router.post("/create", async (req, res) => {
-  const newData = req.body;
+  let newData = req.body;
   const documentsCollection = db.collection(HOME_COLLECTION);
-
-  const newHome = new Home(newData);
-
+  console.log("created with data: ", newData);
   try {
     const docRef = await documentsCollection.add(newData);
-    res.status(201).send(`Document created with ID: ${docRef.id}`);
+    res.status(201).send(docRef.id);
   } catch (error) {
     console.error("Error adding document: ", error);
     res.status(500).send(error.message);
+  }
+});
+
+router.post("/upload/:id", upload.array("images", 10), async (req, res) => {
+  const { id } = req.params;
+  console.log("id: ", id);
+  const documentRef = db.collection(HOME_COLLECTION).doc(id);
+  if (!req.files) {
+    return res.status(400).send("No files uploaded.");
+  }
+
+  const uploadPromises = req.files.map((file) => {
+    const blob = bucket.file(file.originalname);
+    const blobWriter = blob.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    return new Promise((resolve, reject) => {
+      blobWriter.on("error", (err) => reject(err));
+      blobWriter.on("finish", () => {
+        blob
+          .makePublic()
+          .then(() => {
+            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            resolve(publicUrl);
+          })
+          .catch((err) => reject(err));
+      });
+      blobWriter.end(file.buffer);
+    });
+  });
+
+  try {
+    const urls = await Promise.all(uploadPromises);
+    // URLs of all uploaded files
+    console.log(urls);
+    await documentRef.update({ images: urls });
+    res.status(200).send({ urls });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error uploading files");
   }
 });
 
